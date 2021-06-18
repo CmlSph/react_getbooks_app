5d6f8c2f74d2c57e85d22b06ff78949f1b00b70b
CmlSph <sph_cml1974@outlook.com>
2021-06-18 17:24:38 +0200

    react_getbooks_app

---
+++ src/Book.js
@@ -0,0 +1,55 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { BooksContext } from "./App";
+import image from "./images/noCover.jpg";
+
+const Book = (props) => {
+  const { books } = useContext(BooksContext);
+  const { setBooks } = useContext(BooksContext);
+
+  const removeBook = (key, index) => {
+    setBooks(books.filter((data) => data.key !== key));
+  };
+  const { item } = props;
+
+  const linkStyle = {
+    textDecoration: "none",
+    color: "black",
+  };
+  return (
+    <>
+      <div className="bookContainer">
+        <p className="innerContainer" style={linkStyle} key={item.seed[0]}>
+          <button
+            type="button"
+            className="deleteBookButton"
+            onClick={() => removeBook(item.key)}
+          >
+            X
+          </button>
+          {item && item.isbn && item.isbn.length > 0 ? (
+            <img
+              alt=""
+              src={`http://covers.openlibrary.org/b/isbn/${item.isbn[0]}-M.jpg`}
+            />
+          ) : (
+            <img alt="" src={image} />
+          )}
+          <br />
+          <Link to={`${item.seed[0]}`} style={linkStyle}>
+            Title: {item.title} <br />
+            <br />
+            Author: {item.author_name} <br />
+            <br />
+            First Publish Year: {item.first_publish_year} <br />
+            <br />
+            Edition Count: {item.edition_count} <br />
+            <br />
+            Language: {item.language} <br />
+          </Link>
+        </p>
+      </div>
+    </>
+  );
+};
+export default Book;
